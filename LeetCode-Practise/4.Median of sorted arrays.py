5f53ab373c767757b98c5cd7af056ca8ea8d465a
armaango <armaan.goyal@gmail.com>
2016-07-19 18:31:11 -0400

    4th challenge solved - may improve later

---
+++ LeetCode-Practise/4.Median of sorted arrays.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def findMedianSortedArrays(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: float
+        """
+        nums1.extend(nums2)
+        list.sort(nums1)
+        if len(nums1) % 2 == 0:
+            return (nums1[(len(nums1)/2)-1]+nums1[(len(nums1)/2)])/2.0
+        else:
+            return nums1[(len(nums1)/2)]
